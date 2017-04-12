8a98c303c23b2e541dc550144feaea9145f77609
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-04-12 12:37:25 +0200

    create the form without doctrine generation

--- src/DMB/BlogBundle/Controller/DefaultController.php
+++ src/DMB/BlogBundle/Controller/DefaultController.php
@@ -5,6 +5,12 @@ namespace DMB\BlogBundle\Controller;
 use DMB\BlogBundle\Entity\Comment;
 use DMB\BlogBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
 {
@@ -18,6 +24,21 @@ class DefaultController extends Controller
 
     public function postAction($id)
     {
+        $comment = new Comment;
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $comment);
+
+        $formBuilder
+            ->add('date',      DateType::class)
+            ->add('content',   TextareaType::class)
+            ->add('author',    TextType::class)
+            ->add('email',     TextType::class)
+            ->add('save',      SubmitType::class, array(
+                'attr' => array('class' => 'col s2 btn waves-effect waves-light')
+            ))
+        ;
+
+        $form = $formBuilder->getForm();
+
         $em = $this->getDoctrine()->getManager();
 
         $post = $em->getRepository('DMBBlogBundle:Post')->find($id);
@@ -30,7 +51,11 @@ class DefaultController extends Controller
             ->findBy(array('post' => $id))
         ;
 
-        return $this->render('DMBBlogBundle:Default:post.html.twig', compact('post', 'comments'));
+        return $this->render('DMBBlogBundle:Default:post.html.twig', array(
+            'form' => $form->createView(),
+            'post' => $post,
+            'comments' => $comments,
+        ));
 
     }
 }
