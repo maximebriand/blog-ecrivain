bea40f17a5f713f7661258d5cae4016e46323433
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-04-24 22:33:39 +0200

    add image upload for avatar on front end

--- src/DMB/UserBundle/Entity/User.php
+++ src/DMB/UserBundle/Entity/User.php
@@ -3,12 +3,17 @@
 
 namespace DMB\UserBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-
+use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 /**
- * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="DMB\UserBundle\Repository\UserRepository")
+ * @Vich\Uploadable
  */
 class User extends BaseUser
 {
@@ -18,10 +23,69 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    protected $image;
+    /**
+     * @Vich\UploadableField(mapping="avatar_images", fileNameProperty="image")
+     * @var File
+     */
+    protected $imageFile;
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    protected $updatedAt;
 
-    public function __construct()
+
+    public function setImageFile(File $image = null)
     {
-        parent::__construct();
-        // your own logic
+        $this->imageFile = $image;
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
     }
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+    public function getImage()
+    {
+        return $this->image;
+    }
+    public function eraseCredentials()
+    {
+    }
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
 }
---
+++ src/DMB/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DMB\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('imageFile', VichImageType::class, [
+            'attr' => [
+                'class' =>  'file-field input-field'
+            ],
+            'required' => false,
+            'allow_delete' => true, // optional, default is true
+            'download_link' => true, // optional, default is true
+        ]);
+    }
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
