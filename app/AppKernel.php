87c87ab5e1eb8ddf207932f452a023b19079b766
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-04-24 17:49:49 +0200

    WISIWIG in the admin to create post

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new DMB\UserBundle\DMBUserBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
