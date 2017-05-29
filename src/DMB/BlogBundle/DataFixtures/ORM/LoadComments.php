a8725ee1a579426aaf165183be9787dad844c5d3
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-05-29 19:34:22 +0200

    fixtures with lorem comments

--- src/DMB/BlogBundle/DataFixtures/ORM/LoadComments.php
+++ src/DMB/BlogBundle/DataFixtures/ORM/LoadComments.php
@@ -25,15 +25,19 @@ class LoadComments  extends AbstractFixture implements OrderedFixtureInterface, 
         $user_list = $em->getRepository('DMBUserBundle:User')->findAll();
         $post_list = $em->getRepository('DMBBlogBundle:Post')->findAll();
 
-        $comment = new Comment();
-        $comment
-            ->setContent('Jean Forteroche')
-            ->setMember($user_list[rand(0, 9)])
-            ->setDate(new \DateTime('now'))
-            ->setPost($post_list[rand(0, 9)])
-        ;
 
-        $manager->persist($comment);
+        for ($i = 0; $i < 50; $i++)
+        {
+            $comment = new Comment();
+            $comment
+                ->setContent($this->getRandomContent())
+                ->setMember($user_list[rand(0, 9)])
+                ->setDate(new \DateTime('now'))
+                ->setPost($post_list[rand(0, 12)])
+            ;
+            $manager->persist($comment);
+        }
+
         $manager->flush();
     }
 
@@ -43,5 +47,30 @@ class LoadComments  extends AbstractFixture implements OrderedFixtureInterface, 
         // the lower the number, the sooner that this fixture is loaded
         //User must be created first, then chapter and finally comments
         return 3;
+    }
+
+    private function getRandomContent()
+    {
+        $comments = array(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'Pellentesque vitae velit ex.',
+            'Mauris dapibus, risus quis suscipit vulputate, eros diam egestas libero, eu vulputate eros eros eu risus.',
+            'In hac habitasse platea dictumst.',
+            'Morbi tempus commodo mattis.',
+            'Donec vel elit dui.',
+            'Ut suscipit posuere justo at vulputate.',
+            'Phasellus id porta orci.',
+            'Ut eleifend mauris et risus ultrices egestas.',
+            'Aliquam sodales, odio id eleifend tristique, urna nisl sollicitudin urna, id varius orci quam id turpis.',
+            'Nulla porta lobortis ligula vel egestas.',
+            'Curabitur aliquam euismod dolor non ornare.',
+            'Nunc et feugiat lectus.',
+            'Nam porta porta augue.',
+            'Sed varius a risus eget aliquam.',
+            'Nunc viverra elit ac laoreet suscipit.',
+            'Pellentesque et sapien pulvinar, consectetur eros ac, vehicula odio.',
+        );
+
+        return $comments[rand(0, 16)];
     }
 }
