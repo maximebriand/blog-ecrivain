e52eedcfbe6ad2b2904502f20d2247519435b4cf
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-05-30 10:09:00 +0200

    optimize chapterNavigation service

--- src/DMB/BlogBundle/Service/getChapterNavigation.php
+++ src/DMB/BlogBundle/Service/getChapterNavigation.php
@@ -20,8 +20,8 @@ class getChapterNavigation
     public function getChapter($roles, $chapterNumber, $post, $id, $em, $cache)
     {
         if (
-            ($roles->isGranted('ROLE_ADMIN') and $post !== null)
-            or ($post !== null and $post->getPublishedDate() < (new \DateTime(('now'))))
+            ($roles->isGranted('ROLE_ADMIN') && $post !== null)
+            || ($post !== null && $post->getPublishedDate() < (new \DateTime(('now'))))
         ) {
             //generate previous and next chapter
             //if it's an admin we display all chapter navigation button
@@ -29,13 +29,8 @@ class getChapterNavigation
                 $previousChapter = $em->getRepository('DMBBlogBundle:Post')->findByIdChapterNumber($chapterNumber - 1);
                 $nextChapter = $em->getRepository('DMBBlogBundle:Post')->findByIdChapterNumber($chapterNumber + 1);
             } elseif ($roles->isGranted('ROLE_USER')) {//if it's a registered user
-                $key_post_member = md5('posts_member' . $id);
                 $key_post_previous_member = md5('posts_previous_anon' . $id);
                 $key_post_next_member = md5('posts_next_anon' . $id);
-
-                //cache for post
-                $doctrine = $em->getRepository('DMBBlogBundle:Post')->find($id);
-                $post = $cache->checkIfStoredInCache($key_post_member, $doctrine);
 
                 $previousChapter = $cache
                     ->checkIfStoredInCache($key_post_previous_member,
@@ -51,13 +46,8 @@ class getChapterNavigation
                     );
             } else //if it's anon user
             {
-                $key_post_anon = md5('posts_anon' . $id);
                 $key_post_previous_anon = md5('posts_previous_anon' . $id);
                 $key_post_next_anon = md5('posts_next_anon' . $id);
-
-                //cache for post
-                $doctrine = $em->getRepository('DMBBlogBundle:Post')->find($id);
-                $post = $cache->checkIfStoredInCache($key_post_anon, $doctrine);
 
                 $previousChapter = $cache
                     ->checkIfStoredInCache($key_post_previous_anon,
